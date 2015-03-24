4574a91e529e69e61a44e2ba3d078688adab8f7c
阿部信太郎 <shintarowfresh@gmail.com>
2015-03-24 18:39:11 +0900

    テンプレートに最低限必要なもの

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title><?php wp_title(); ?></title>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div id="wrapper" class="wrapper">
+
+</div><!--/.wrapper-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
